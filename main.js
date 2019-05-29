d30970faeb44f68ce779f031e3329cf35b4e5114
Robby Renz <nxgrenz@gmail.com>
2019-05-29 16:55:04 -0500

    individual keys are now highlighted as the user presses the corresponding key

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 window.addEventListener("keydown", playSound)
+window.addEventListener("keydown", highlightKeyboard)
+
+const keys = document.querySelectorAll(".key")
+keys.forEach(key => key.addEventListener("transitionend", removeTransition))
 
 function playSound(e) {
   const audio = document.querySelector('audio')
   audio.currentTime = 0
   audio.play();
+}
+
+function highlightKeyboard(e) {
+  e.preventDefault()
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+  // console.log(key)
+  key.classList.add("pressed")
+}
+
+function removeTransition() {
+  this.classList.remove('pressed')
 }
